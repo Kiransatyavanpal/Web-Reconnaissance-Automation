16c187e768967be399df341c8249643bbb31ef29
Kiran Pal <84024950+Kiransatyavanpal@users.noreply.github.com>
2023-03-19 03:05:54 +0530

    Create insert.php

---
+++ Frontend/insert.php
@@ -0,0 +1,60 @@
+<?php
+if (isset($_POST['submit'])) {
+    if (isset($_POST['email']) && isset($_POST['url']))
+    {
+        
+      
+        $email = $_POST['email'];
+        $url = $_POST['url'];
+       
+
+        $host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "web";
+
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
+        }
+        else {
+            $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
+            $Insert = "INSERT INTO register(email, url) values(?, ?)";
+
+            $stmt = $conn->prepare($Select);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($resultEmail);
+            $stmt->store_result();
+            $stmt->fetch();
+            $rnum = $stmt->num_rows;
+
+            if ($rnum == 0) {
+                $stmt->close();
+
+                $stmt = $conn->prepare($Insert);
+                $stmt->bind_param("ssssii",$email, $url);
+                if ($stmt->execute()) {
+                    echo "New record inserted sucessfully.";
+                }
+                else {
+                    echo $stmt->error;
+                }
+            }
+            else {
+                echo "Someone already registers using this email.";
+            }
+            $stmt->close();
+            $conn->close();
+        }
+    }
+    else {
+        echo "All field are required.";
+        die();
+    }
+}
+else {
+    echo "Submit button is not set";
+}
+?>
